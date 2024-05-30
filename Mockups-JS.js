ad7f117941cf38e6cf81539fea965ccace3f4497
moakem <keepingitrealwithmm@gmail.com>
2024-05-29 22:30:13 -0600

    Update Mockups-JS.js

--- Mockups-JS.js
+++ Mockups-JS.js
@@ -154,10 +154,8 @@ function showNav(style) {
 	}
 	else if (style == "Moroccan" || style == "Kite" || style == "Cairo" || style == "Oakley" || style == "Hoop"){
 		document.getElementById("navigation").style.display = "none";
-		document.getElementById("medium").style.display = "none";
-		document.getElementById("small").style.display = "none";
-		document.getElementById("pattern1_8_3").style.display = "none";
-		document.getElementById("pattern2_8_3").style.display = "none";
+		document.getElementById("large").style.display = "block";
+		document.getElementById("pattern0_8_3").style.display = "block";
 	}
 	else {
 		document.getElementById("navigation").style.display = "none";
