17d883babe79d50643147502559d77338bdc5d6a
moakem <keepingitrealwithmm@gmail.com>
2024-06-01 01:35:59 -0600

    Update Mockups-JS.js

--- Mockups-JS.js
+++ Mockups-JS.js
@@ -245,8 +245,10 @@ function layers(type){
 		var children = document.querySelectorAll('li');
 		document.getElementById("large").style.display = "block";
 		document.getElementById("pattern0_8_3").style.display = "block";
+		document.getElementById("regular").style.display = "block";
 		document.getElementById("medium").style.display = "none";
 		document.getElementById("pattern1_8_3").style.display = "none";
+		document.getElementById("mini").style.display = "none";
 		document.getElementById("mockup").style.height = "";
 		children.forEach(function(e) {
 	  		e.classList.remove('selected');
@@ -257,8 +259,10 @@ function layers(type){
 		var children = document.querySelectorAll('li');
 		document.getElementById("large").style.display = "none";
 		document.getElementById("pattern0_8_3").style.display = "none";
+		document.getElementById("regular").style.display = "none";
 		document.getElementById("medium").style.display = "block";
 		document.getElementById("pattern1_8_3").style.display = "block";
+		document.getElementById("mini").style.display = "block";
 		document.getElementById("mockup").style.height = "";
 		const hardwoodColors = document.querySelectorAll('.medium hardwood');
 		for (let i = 0; i < hardwoodColors.length; i++) {
