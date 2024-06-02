b0de55037dbd937272831d9ae22f97d483967554
moakem <keepingitrealwithmm@gmail.com>
2024-06-01 23:29:52 -0600

    Update Mockups-JS.js

--- Mockups-JS.js
+++ Mockups-JS.js
@@ -7,15 +7,9 @@ function tryIt (element){
   		e.classList.remove('selected');
 	});
 	element.classList.add("selected");
-	var currentUrl = window.location.href;
-	    let paramString = currentUrl.split('/')[3];
-		document.getElementById("lSelected").innerHTML = selectedColorName;
-	    if (paramString == "RochelleCustomization.html"){
-    		document.getElementById("image083").setAttribute("xlink:href", "Colors/" + selectedColor + ".jpg");
-    		}
-	else{
+	document.getElementById("lSelected").innerHTML = selectedColorName;
 	document.getElementById("image0_8_3").setAttribute("xlink:href", "Colors/" + selectedColor + ".jpg");
-	}
+
 }
 
 
@@ -29,15 +23,8 @@ function tryIt2 (element){
   		e.classList.remove('selected');
 	});
 	element.classList.add("selected");
-var currentUrl = window.location.href;
-	    let paramString = currentUrl.split('/')[3];
 	document.getElementById("mSelected").innerHTML = selectedColorName;
-	    if (paramString == "RochelleCustomization.html"){
-    		document.getElementById("image183").setAttribute("xlink:href", "Colors/" + selectedColor + ".jpg");
-    }
-	else{
 	document.getElementById("image1_8_3").setAttribute("xlink:href", "Colors/" + selectedColor + ".jpg");
-	}
 }
 
 function tryIt3 (element){
