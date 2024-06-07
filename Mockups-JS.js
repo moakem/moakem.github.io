777c4afe7273182116c4eeaf7bed9cdc75b88033
moakem <keepingitrealwithmm@gmail.com>
2024-06-06 18:40:05 -0600

    Update Mockups-JS.js

--- Mockups-JS.js
+++ Mockups-JS.js
@@ -90,6 +90,7 @@ function tryIt4 (element){
 			document.getElementById("mHSelected").style.display = "none";
 			document.getElementById("image1_8_3").setAttribute("xlink:href", "Colors/" + selectedColor + ".jpg");
 			document.getElementsByTagName("svg")[0].children[1].setAttribute("stroke", "black");
+			document.getElementsByTagName("svg")[0].children[3].setAttribute("stroke", "black");
 		}
 	}
 }
